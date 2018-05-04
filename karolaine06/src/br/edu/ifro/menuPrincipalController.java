fde8fbca5aa26c70078c0a42f76efd9ae2f49f52
04225321266 <04225321266@JIPA-LABIN03-28.ifro.local>
2018-05-04 17:26:36 -0400

    continuar video 4 pro 5

---
+++ karolaine06/src/br/edu/ifro/menuPrincipalController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro;
+
+import java.net.URL;
+
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author 04225321266
+ */
+public class menuPrincipalController implements Initializable {
+    
+    @FXML
+    private TextField txtNumero1, txtNumero2, txtResultado;
+    
+    @FXML
+    private Button  btnSoma;
+    
+    @FXML
+    private void soma(ActionEvent event) {
+       Double num1 = Double.parseDouble(txtNumero1.getText());
+       Double num2 = Double.parseDouble(txtNumero2.getText());
+       Double resultado = num1 + num2;
+    
+       txtResultado.setText(resultado.toString());
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      
+    }    
+    
+}
